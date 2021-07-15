337844c51221ae354ec88c5e80dddb5c27947f49
DanielleE1998 <danielle.mawson.1998@gmail.com>
2021-07-15 02:25:01 -0400

    fixed header, added to homepage

---
+++ Scripts/scripts.js
@@ -0,0 +1,10 @@
+//Owl Carousel
+$('js-carousel').owlCarousel({
+    loop: true,
+    autoplay: true,
+    items: 1,
+    navText: ["<span class='fa fa-chevron-left'></span>",
+    "<span class='fa fa-chevron-right'></span>"],
+    responsive:{
+    
+})
